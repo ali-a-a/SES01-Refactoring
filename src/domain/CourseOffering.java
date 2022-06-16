f6a0fb7cad2facfb9d7a900b1e4a470ca545f3af
ali abbasi <ali.abbasialaei@snapp.cab>
2022-06-15 23:47:50 -0700

    Format code

--- src/domain/CourseOffering.java
+++ src/domain/CourseOffering.java
@@ -1,40 +1,43 @@
 package domain;
+
 import java.util.Date;
 
 public class CourseOffering {
-	private Course course;
-	private int section;
-	private Date examDate;
+    private Course course;
+    private int section;
+    private Date examDate;
 
-	public CourseOffering(Course course) {
-		this.course = course;
-		this.section = 1;
-		this.examDate = null;
-	}
+    public CourseOffering(Course course) {
+        this.course = course;
+        this.section = 1;
+        this.examDate = null;
+    }
 
-	public CourseOffering(Course course, Date examDate) {
-		this.course = course;
-		this.section = 1;
-		this.examDate = examDate;
-	}
+    public CourseOffering(Course course, Date examDate) {
+        this.course = course;
+        this.section = 1;
+        this.examDate = examDate;
+    }
 
-	public CourseOffering(Course course, Date examDate, int section) {
-		this.course = course;
-		this.section = section;
-		this.examDate = examDate;
-	}
-	
-	public Course getCourse() {
-		return course;
-	}
-	
-	public String toString() {
-		return course.getName() + " - " + section;
-	}
-	
-	public Date getExamTime() {
-		return examDate;
-	}
+    public CourseOffering(Course course, Date examDate, int section) {
+        this.course = course;
+        this.section = section;
+        this.examDate = examDate;
+    }
 
-	public int getSection() { return section; }
+    public Course getCourse() {
+        return course;
+    }
+
+    public String toString() {
+        return course.getName() + " - " + section;
+    }
+
+    public Date getExamTime() {
+        return examDate;
+    }
+
+    public int getSection() {
+        return section;
+    }
 }
