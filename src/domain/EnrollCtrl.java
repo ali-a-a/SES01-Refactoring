0bfa9e325e151f7c950d8aca8a225aecede30ee5
Javad <javad.zahedi2001@gmail.com>
2022-06-17 22:25:15 +0430

    Handling multiple error

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -7,32 +7,36 @@ import domain.exceptions.EnrollmentRulesViolationException;
 
 public class EnrollCtrl {
     public void enroll(Student s, List<CourseOffering> courseOfferings) throws EnrollmentRulesViolationException {
+        StringBuilder errorMessage = new StringBuilder();
         for (CourseOffering o : courseOfferings) {
             Course offeredCourse = o.getCourse();
             if (s.isPassed(offeredCourse))
-                throw new EnrollmentRulesViolationException(String.format("The student has already passed %s", o.getCourse().getName()));
+                errorMessage.append(String.format("The student has already passed %s\n", o.getCourse().getName()));
 
             List<Course> prerequisites = offeredCourse.getPrerequisites();
             for (Course pre : prerequisites)
                 if (!s.isPassed(pre))
-                    throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s", pre.getName(), o.getCourse().getName()));
+                    errorMessage.append(String.format("The student has not passed %s as a prerequisite of %s\n", pre.getName(), o.getCourse().getName()));
 
             for (CourseOffering o2 : courseOfferings) {
                 if (o == o2)
                     continue;
                 if (o.hasOverlapWith(o2))
-                    throw new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam time", o, o2));
+                    errorMessage.append(String.format("Two offerings %s and %s have the same exam time\n", o, o2));
                 if (o.hasSameCourseWith(o2))
-                    throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice", o.getCourse().getName()));
+                    errorMessage.append(String.format("%s is requested to be taken twice\n", o.getCourse().getName()));
             }
         }
 
         double gpa = s.GPA();
         int unitsRequested = CourseOffering.sumOfUnits(courseOfferings);
         if (unitsRequested > maxAllowedUnits(gpa))
-            throw new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
+            errorMessage.append(String.format("Number of units (%d) requested does not match GPA of %f\n", unitsRequested, gpa));
 
-        s.takeCourses(courseOfferings);
+        if (errorMessage.isEmpty())
+            s.takeCourses(courseOfferings);
+        else
+            throw new EnrollmentRulesViolationException(errorMessage.toString());
     }
 
     static int maxAllowedUnits(double gpa) {
